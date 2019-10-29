11ffdcbb9635a46c50a9f7cd05be3965d4f2302a
Nick Tardif <nicktardif@gmail.com>
2019-10-29 23:14:23 +0000

    Make the APIHandler errors more clear

--- docker_api_testing/docker_api_handler.py
+++ docker_api_testing/docker_api_handler.py
@@ -9,33 +9,30 @@ class DockerAPIHandler():
         self.session = requests_unixsocket.Session()
 
     def get(self, endpoint, data):
-        response = None
         try:
-            response = self.session.get(
+            return self.session.get(
                     config.docker_socket + config.docker_version + endpoint,
                     data=data)
         except ConnectionError as e:
-            pass
-        return response
+            print('Error: Could not establish connection to Docker daemon')
+        return None
 
     def post(self, endpoint, data):
-        response = None
         try:
             headers = {'Content-type': 'application/json'}
-            response = self.session.post(
+            return self.session.post(
                     config.docker_socket + config.docker_version + endpoint,
                     data=data,
                     headers=headers,)
         except ConnectionError as e:
-            pass
-        return response
+            print('Error: Could not establish connection to Docker daemon')
+        return None
 
     def delete(self, endpoint, data):
-        response = None
         try:
-            response = self.session.delete(
+            return self.session.delete(
                     config.docker_socket + config.docker_version + endpoint,
                     data=data,)
         except ConnectionError as e:
-            pass
-        return response
+            print('Error: Could not establish connection to Docker daemon')
+        return None
